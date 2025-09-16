174be0918c600cfce9b9ca0fcaf16d6e3eb36797
mariajose <mariajosegrauvidal@gmail.com>
2025-09-16 16:50:52 +0200

    Introducir isNaN

--- vanilla/bibliotecaf/funcionesEjercicios.js
+++ vanilla/bibliotecaf/funcionesEjercicios.js
@@ -2,10 +2,33 @@
 // Funciones para los ejercicios de la práctica 2-01
 //Ejercicio1
 function calcularIMC (peso, altura) {
-    return peso / (altura * altura);
+    if(isNaN(peso) || isNaN(altura)){
+        
+        return console.log("Porfavor, introduce un dato válido");
+        
+    }else{
+        return peso / (altura * altura);
+        
+    }
+
+    
 };
-//Ejercicio2
+
+const imcMayor = (imc1, imc2) =>{
+   //et mayor = 0;
+    if(imc1 > imc2){
+       //ayor = imc1;
+        return imc1;
+    }else{
+        return imc2;
+
+    }
+}
+
+
+
 
 
 //exports
 export { calcularIMC };
+export {imcMayor};
--- vanilla/src/Practica2-01/ejercicio1/main.js
+++ vanilla/src/Practica2-01/ejercicio1/main.js
@@ -1,15 +1,18 @@
 "use estrict"
 import {calcularIMC} from "../../../bibliotecaf/funcionesEjercicios.js";
+import {imcMayor} from "../../../bibliotecaf/funcionesEjercicios.js";
 
-const pesoJuan = 70; // en kg
-const alturaJuan = 1.75; // en metros
+const pesoJuan = "hola" // en
+const alturaJuan = 1.75;// en cm
 const pesoMarcos = 95; // en kg
-const alturaMarcos = 1.8; // en metros
+const alturaMarcos = 1.80;// en cm
 
 const imcJuan = calcularIMC(pesoJuan, alturaJuan);
-//console.log(`El IMC de Juan es: ${imcJuan}`);
+console.log(`El IMC de Juan es: ${imcJuan}`);
 
 const imcMarcos = calcularIMC(pesoMarcos, alturaMarcos);
-//console.log(`El IMC de Marcos es: ${imcMarcos}`);
+console.log(`El IMC de Marcos es: ${imcMarcos}`);
+
+console.log(imcMayor(imcMarcos, imcJuan));
 
 console.log(`¿El IMC de Marcos es mayor que el de Juan? ${imcMarcos > imcJuan ? "Sí" : "No"}`);
