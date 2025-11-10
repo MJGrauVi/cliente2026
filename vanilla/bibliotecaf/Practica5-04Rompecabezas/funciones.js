0346b78dea93d12ec3665a87467cea35d7de3bff
MJGrauVi <mariajosegrauvidal@gmail.com>
2025-11-10 09:36:43 +0100

    add eventos

--- vanilla/bibliotecaf/Practica5-04Rompecabezas/funciones.js
+++ vanilla/bibliotecaf/Practica5-04Rompecabezas/funciones.js
@@ -9,14 +9,71 @@ const cargarImagenes = () => {
   const contenedorImg = document.getElementById("contenedorImg");
   const arrayImagenes = [...Array(9)].map((_, i) => `${i + 1}.png`); //Creo un array vacio y agrego los nombres de las img.
   let imagenesMezcladas = mezclarImagenes(arrayImagenes);
-  
+
   imagenesMezcladas.forEach((imagenArray) => {
     const img = document.createElement("img");
+    
     img.src = `rompecabezas_imagenes/${imagenArray}`; //Recoge la imagen del directorio.
     img.alt = "imagen-rompecabezas";
     img.classList.add("imagen");
+    img.setAttribute("draggable", true);
     contenedorImg.appendChild(img);
   });
 };
+const celdasArrastables = document.getElementsByClassName("celda");
+for (let i = 0; i < celdasArrastables.length; i++) {
+  celdasArrastables[i].getAttribute("draggable", true);
+}
 
+document.getElementById("contenedorImg").addEventListener(
+  "dragstart",
+  (evento) => {
+    console.log(evento);
+    //Configurar el identificador del elemento a arrastrar.
+    evento.dataTransfer.setData("identificador", evento.target.id);
+    // Se muestran los datos por consola.
+    console.log(evento.dataTransfer.getData("identificador"));
+    console.log(evento.dataTransfer.getData("nombre"));
+  },
+  false
+);
+document.getElementById("contenedorImg").addEventListener(
+  "dragover",
+  (evento) => {
+    evento.preventDefault(); //Previene el comportamiento del navegador.
+  },
+  false
+);
+document.getElementById("contenedorPuzle").addEventListener(
+  "dragstart",
+  (evento) => {
+    console.log(evento);
+    //Configurar el identificador del elemento a arrastrar.
+    evento.dataTransfer.setData("identificador", evento.target.id);
+    // Se muestran los datos por consola.
+    console.log(evento.dataTransfer.getData("identificador"));
+    console.log(evento.dataTransfer.getData("nombre"));
+  },
+  false
+);
+document.getElementById("contenedorPuzle").addEventListener(
+  "dragover",
+  (evento) => {
+    evento.preventDefault("");
+  },
+  false
+);
+document.getElementById("contenedorPuzle").addEventListener(
+  "drop",
+  (evento) => {
+    evento.preventDefault();
+    if (evento.target.classList.contains("celda")) {
+      console.log(`Suelto imagen: ${evento.target.className}`);
+      evento.target.appendChild(
+        document.getElementById(evento.dataTransfer.getData("identificador"))
+      );
+    }
+  },
+  false
+);
 export { cargarImagenes, mezclarImagenes };
--- vanilla/bibliotecaf/Practica5-04Rompecabezas/main.js
+++ vanilla/bibliotecaf/Practica5-04Rompecabezas/main.js
@@ -5,7 +5,57 @@ import {cargarImagenes} from "./funciones.js";
 window.addEventListener("DOMContentLoaded", () => {
 
   cargarImagenes();
-
+document.getElementById("contenedorImg").addEventListener(
+  "dragstart",
+  (evento) => {
+    console.log(evento);
+    //Configurar el identificador del elemento a arrastrar.
+    evento.dataTransfer.setData("identificador", evento.target.id);
+    // Se muestran los datos por consola.
+    console.log(evento.dataTransfer.getData("identificador"));
+    console.log(evento.dataTransfer.getData("nombre"));
+  },
+  false
+);
+document.getElementById("contenedorImg").addEventListener(
+  "dragover",
+  (evento) => {
+    evento.preventDefault(); //Previene el comportamiento del navegador.
+  },
+  false
+);
+document.getElementById("contenedorPuzle").addEventListener(
+  "dragstart",
+  (evento) => {
+    console.log(evento);
+    //Configurar el identificador del elemento a arrastrar.
+    evento.dataTransfer.setData("identificador", evento.target.id);
+    // Se muestran los datos por consola.
+    console.log(evento.dataTransfer.getData("identificador"));
+    console.log(evento.dataTransfer.getData("nombre"));
+  },
+  false
+);
+document.getElementById("contenedorPuzle").addEventListener(
+  "dragover",
+  (evento) => {
+    evento.preventDefault("");
+  },
+  false
+);
+document.getElementById("contenedorPuzle").addEventListener(
+  "drop",
+  (evento) => {
+    evento.preventDefault();
+    if (evento.target.classList.contains("celda")) {
+      console.log(`Suelto imagen: ${evento.target.className}`);
+      evento.target.appendChild(
+        document.getElementById(evento.dataTransfer.getData("identificador"))
+      );
+    }
+  },
+  false
+);
    
 
-});//fin.
+});//fin del addEventListener.
