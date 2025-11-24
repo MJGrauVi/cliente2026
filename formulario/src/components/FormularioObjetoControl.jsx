2669ba8098c6ea230a2958a5f2eb809b3891af96
MJGrauVi <mariajosegrauvidal@gmail.com>
2025-11-24 16:21:57 +0100

    prestado

--- formulario/src/components/FormularioObjetoControl.jsx
+++ formulario/src/components/FormularioObjetoControl.jsx
@@ -6,20 +6,32 @@ function FormularioObjetoControl() {
   // Se crea un estado inicial (objeto con valores por defecto para evitar errores al dibujar).
   const valoresIniciales = {
     nombre: "",
-    apellidos: "",
-    correo: "",
+    caratula: "",
+    grupoOsolista: "",
+    anio: "",
+    generoMusica: "",
+    localizacion: "",
+    prestado: "",
   };
-
+  //Inicializo array vacio para ir añadiendo los errores de validación.
   const erroresIniciales = [];
-  // Estado para los valores del disco.
-  const [disco, setdisco] = useState(valoresIniciales);
-  // Estado para los errores (en este caso un array de cadenas de texto).
+
+  // Estado para los valores del disco, inicialmente cadenas vacias.
+  const [disco, setDisco] = useState(valoresIniciales);
+
   const [errores, setErrores] = useState(erroresIniciales);
 
-  // Función para actualizar el estado con los datos del evento (ya conocida).
+  // Función para actualizar el estado con los datos del evento.
   const actualizarDato = (evento) => {
     const { name, value } = evento.target;
-    setdisco({ ...disco, [name]: value });
+    setDisco({ ...disco, [name]: value });
+  };
+  // La cosa empieza a complicarse con los elementos checkbox,
+  // por eso se hace una función especial para este elemento.
+  const actualizarDatoCheck = (evento) => {
+    const { name } = evento.target;
+    const value = disco[name] === "" ? evento.target.value : "";
+    setDisco({ ...disco, [name]: value });
   };
 
   // Función que valida el valor de un input.
@@ -29,7 +41,7 @@ function FormularioObjetoControl() {
     // Variable con los errores de cada elemento.
     let erroresElemento = [];
     // Comprobaciones para cada uno de los elementos del formulario.
-    // Campo nombre.
+    /* Campo nombre --------------------------------------------------------------*/
     if (name === "nombre") {
       // Se comprueba si tiene algo escrito.
       if (!value.length) {
@@ -39,13 +51,24 @@ function FormularioObjetoControl() {
         ];
       }
       // Se comprueba que el valor tenga al menos 5 caracteres máximo 21. /* visto */
-      if (!/^[a-zA-Z][a-zA-Z0-9 ]{4,20}$/.test(value))
+      if (!/^[a-zA-Z][a-zA-Z0-9 ]{4,20}$/.test(value)) {
         erroresElemento = [
           ...erroresElemento,
           `El nombre debe tener entre dos y veinte caracteres y no puede comenzar con un número.`,
         ];
+      }
     }
-    // Campo año.
+    /* Campo caratula -----------------------------------------------------------*/
+    if (name === "caratula") {
+      // Se comprueba si tiene algo escrito.
+      if (!value.length)
+        erroresElemento = [
+          ...erroresElemento,
+          `El campo ${name} debe tener un valor.`,
+        ];
+    }
+
+    /* Campo año ---------------------------------------------.------------------*/
     if (name === "anio") {
       // Se comprueba si tiene algo escrito.
       if (!value.length)
@@ -57,7 +80,7 @@ function FormularioObjetoControl() {
       if (!/^\d{4}$/.test(value))
         erroresElemento = [...erroresElemento, `El año debe tener 4 dígitos.`];
     }
-    // Campo correo electrónico.
+    // Campo correo electrónico----------------------------------------------------------*/
     if (name === "correo") {
       // Se comprueba si tiene algo escrito.
       if (!value.length)
@@ -114,7 +137,19 @@ function FormularioObjetoControl() {
               }}
             />
             <br />
-
+            <label htmlFor="caratula">Carátula:</label>
+            <input
+              name="caratula"
+              id="caratula"
+              className="conEstilo"
+              type="text"
+              placeholder="Introduce la URL de la carátuala del disco."
+              value={disco.caratula}
+              onChange={(evento) => {
+                actualizarDato(evento);
+              }}
+            />
+            <br />
             <h3>Acceso a Grupo o solista</h3>
             <label>
               <input
@@ -123,11 +158,11 @@ function FormularioObjetoControl() {
                 type="radio"
                 // El valor no se obtiene esta vez desde el estado,
                 // ya que el valor será seleccionado (valores finitos).
-                value="sí"
+                value="grupoMusica"
                 // Checked sólo admite un booleano. Se obtiene del
                 // resultado de comprobar si su valor ("sí" en este caso)
                 // coicide con el valor del estado.
-                checked={disco.grupoOsolista === "sí"}
+                checked={disco.grupoOsolista === "grupoMusica"}
                 onChange={(evento) => {
                   actualizarDato(evento);
                 }}
@@ -140,8 +175,8 @@ function FormularioObjetoControl() {
                 name="grupoOsolista"
                 id="solista"
                 type="radio"
-                value="sí"
-                checked={disco.grupoOsolista === "sí"}
+                value="solista"
+                checked={disco.grupoOsolista === "solista"}
                 onChange={(evento) => {
                   actualizarDato(evento);
                 }}
@@ -149,6 +184,7 @@ function FormularioObjetoControl() {
               Solista
             </label>
             <br />
+            <h3>Lanzamiento del disco</h3>
             <label htmlFor="anio">Año lanzamiento:</label>
             <input
               name="anio"
@@ -163,18 +199,81 @@ function FormularioObjetoControl() {
             />
             <br />
 
+            <h3>Acceso a múltiples checkbox</h3>
+            <label htmlFor="generoMusica">
+              <input
+                name="pop"
+                id="pop"
+                type="checkbox"
+                value="pop"
+                checked={disco.pop}
+                onChange={(evento) => {
+                  actualizarDatoCheck(evento);
+                }}
+              />
+              Pop
+            </label>
+            <label>
+              <input
+                name="rock"
+                type="checkbox"
+                id="rock"
+                value="rock"
+                checked={disco.rock}
+                onChange={(evento) => {
+                  actualizarDatoCheck(evento);
+                }}
+              />
+              Rock
+            </label>
+            <label>
+              <input
+                name="salsa"
+                type="checkbox"
+                id="salsa"
+                value="salsa"
+                checked={disco.salsa}
+                onChange={(evento) => {
+                  actualizarDatoCheck(evento);
+                }}
+              />
+              Salsa
+            </label>
             <br />
-            <label htmlFor="modulos">Correo:</label>
-            <input
-              name="correo"
-              className="conEstilo"
-              type="text"
-              placeholder="Escribe tu correo electrónico."
-              value={disco.correo}
-              onChange={(evento) => {
-                actualizarDato(evento);
-              }}
-            />
+            <label>
+              <input
+                name="clasica"
+                type="checkbox"
+                id="clasica"
+                value="clasica"
+                checked={disco.clasica}
+                onChange={(evento) => {
+                  actualizarDatoCheck(evento);
+                }}
+              />
+              Clásica
+            </label>
+            <br />
+            <h3>Préstado</h3>
+            <label>
+              <input
+                name="prestado"
+                type="checkbox"
+                id="prestado"
+                value="si"
+                // Opción para una cadena de texto.
+                // Si la comparación es correcta, se marca el checkbox.
+                checked={disco.prestado === "si"}
+                onChange={(evento) => {
+                  setDisco({
+                    ...disco,
+                    prestado: evento.target.checked ? "sí" : "",
+                  });
+                }}
+              />
+              Prestado
+            </label>
+
             <br />
             <input
               type="button"
